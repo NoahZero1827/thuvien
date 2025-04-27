64d62e4d4c6a862e0f54e3915042ed5ceff1f9db
Ly Phuc Hoa <NoahZero1827@gmail.com>
2025-04-27 22:01:35 +0700

    Cập nhật server.js

--- server.js
+++ server.js
@@ -8,12 +8,10 @@ app.use(cors());
 app.use(express.json());
 
 // Kết nối MongoDB
-mongoose.connect('mongodb+srv://noahzero1827:Lyphuchoa1827@cluster0.rjswawl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.error('Error connecting to MongoDB:', err));
+mongoose.connect('mongodb+srv://noahzero1827:Lyphuchoa1827@cluster0.rjswawl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
 
 // Schema sách
 const bookSchema = new mongoose.Schema({
